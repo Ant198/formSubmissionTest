fb3eb00b66ed0ae2b8585c8c251d68b042ab8f50
Anton <ridnovir@yahoo.com>
2025-03-14 16:29:29 +0200

    add

--- src/main/java/com/demo/core/base/BaseTest.java
+++ src/main/java/com/demo/core/base/BaseTest.java
@@ -24,7 +24,6 @@ public class BaseTest extends AllureLogger {
         SelenideConfig.createBrowserConfig(System.getProperty("selenide.browser", "chrome"));
         configLog(this.getClass().getSimpleName());
         logInfo("Open browser...");
-        Selenide.
         Selenide.open();
     }
 
